4e31a01eb6d1de83943f54f674ecfd752911d73e
Jason Cauwels <cauwels@usc.edu>
2022-09-30 14:00:07 -0700

    feat: get lists of org ids and site names

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
-const terminusUpstreamID = process.env.TERMINUS_UPSTREAM_ID,
-terminusOrgID = process.env.TERMINUS_ORG_ID;
-
+// Require utilities
 const util = require('node:util');
 const exec = util.promisify(require('node:child_process').exec);
 
+// Set variables from shell environment for Organization and Upstream IDs
+const terminusUpstreamID = process.env.TERMINUS_UPSTREAM_ID,
+terminusOrgID = process.env.TERMINUS_ORG_ID;
+
+// Order in which we need to get site information:
 // 1. terminus org:site:list <org-id> --upstream=<upstream-id> --format=json
 // 2. terminus env:list --field=domain <site-id>
 // 3. terminus domain:list --format FORMAT --fields FIELDS --field FIELD -- <site>.<env>
@@ -17,6 +20,12 @@ async function orgSiteList() {
 }
 
 // 2. terminus env:list --field=domain <site-id>
+
+/**
+ * Get site environments available by ID.
+ * @param {string} siteID
+ * @returns object
+ */
 async function siteEnvironments(siteID) {
 	return new Promise(resolve => {
 		resolve( exec(`terminus env:list --field=domain --format=json ${siteID}`) );
@@ -24,44 +33,16 @@ async function siteEnvironments(siteID) {
 }
 
 // 3. terminus domain:list --format FORMAT --fields FIELDS --field FIELD -- <site>.<env>
+/**
+ * Get site domains by base sitename.
+ * @param {string} siteName
+ * @returns object
+ */
 async function siteDomains(siteName) {
 	return new Promise(resolve => {
 		resolve( exec(`terminus domain:list --fields=id --format=json -- ${siteName}.live`) );
 	});
 }
-
-async function getSite(data, type) {
-
-}
-
-function isObject(val) {
-	if (val === null) {
-		return false;
-	}
-
-	return typeof val === 'object';
-}
-
-// async function nestedObject(obj) {
-// 	let newOBJ = new Object;
-
-// 	for (const key in obj) {
-// 		if (isObject(obj[key])) {
-// 			nestedObject(obj[key]);
-// 		} else {
-// 			// newOBJ.key = obj[key];
-// 			// console.log(obj[key]);
-// 			// console.log(`${key}: ${obj[key]}`);
-// 			// newOBJ.`${key}` = `${obj[key]}`;
-// 			newOBJ[key] = obj[key];
-
-// 		}
-// 		// console.log(newOBJ);
-// 	}
-// 	// console.log(newOBJ);
-// 	return newOBJ;
-// }
-
 
 
 async function allSites() {
@@ -81,57 +62,23 @@ async function allSites() {
 
 		const orgSites = JSON.parse(result.stdout);
 
-		// console.log(orgSites);
+		// Transform single object to array of site objects
+		const entries = Object.entries(orgSites);
 
+		for (const entry of entries) {
 
-
-		// console.log(orgSites);
-
-		// orgSites.foreach(function(element) {
-		// 	element.environments = siteEnvironments(element.id);
-		// });
-
-		// const objJSON = await nestedObject(orgSites);
-
-		// console.log(orgSites);
-
-		// console.log(objJSON);
-
-		const nestedObject = async (obj) => {
-		for (const key in obj) {
-			if (isObject(obj[key])) {
-				nestedObject(obj[key]);
-			} else {
-				if ( 'name' === key ) {
-					// names.push(obj[key]);
-					let domains = await siteDomains(obj[key]);
-					console.log(domains);
-					// console.log(JSON.parse(domains.stdout));
-					// let list = JSON.parse(domains.stdout);
-					// obj.domains = list;
-
-				}
-				if ( 'id' === key ) {
-					// ids.push(obj[key]);
-					let env = await siteEnvironments(obj[key]);
-					console.log(env);
-					// console.log(JSON.parse(env.stdout));
-					// let list = JSON.parse(env.stdout);
-					// obj.environments = list;
-				}
-
+			if ( undefined !== entry[1].name ) {
+				names.push(entry[1].name);
+			}
+			if ( undefined !== entry[1].id ) {
+				ids.push(entry[1].id);
 			}
 		}
-		//   console.log(obj);
-		};
-
-		// Iterate over nested objects
-		nestedObject(orgSites);
 
 	}
 
-	// console.log(names);
-	// console.log(ids);
+	console.log(names);
+	console.log(ids);
 
 
 	// Get individual environments
